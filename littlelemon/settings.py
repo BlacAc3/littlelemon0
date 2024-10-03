d6247c02f44bb2464930236378f1f0df52ca5708
BlacAc3 <aceezeala@gmail.com>
2024-10-03 22:56:56 +0100

    fixing depl error

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # ]
 
 STATIC_URL = '/staticfiles/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_FILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
