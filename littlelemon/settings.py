f25e93c7524a652acac5ced4fe9bec1ca10da4c4
BlacAc3 <aceezeala@gmail.com>
2024-10-03 22:51:11 +0100

    adjusted env getter

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r-%0eqcja86asp_+*1@3d6t9wm65$7^p*v^m961p@)w7*3ob0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['littlelemon-y7q1.onrender.com', '127.0.0.1', 'localhost', '*']
 
